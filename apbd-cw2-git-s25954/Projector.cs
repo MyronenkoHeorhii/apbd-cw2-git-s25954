a9086e5923d1415d72950b0934ba35494ea00b54
Heorhii Myronenko <s25954@pjwstk.edu.pl>
2026-03-23 01:47:18 +0100

    Implemented equipment child class Projector

---
+++ apbd-cw2-git-s25954/Projector.cs
@@ -0,0 +1,14 @@
+﻿namespace apbd_cw2_git_s25954;
+
+public class Projector
+{
+    public string projectorType { get; set; }
+    public string resolution { get; set; }
+    
+    public Projector(string projectorType, string resolution)
+    {
+        this.projectorType = projectorType;
+        this.resolution = resolution;
+    }
+
+}
