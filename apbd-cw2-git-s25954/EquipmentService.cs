38fc3e6939f60def38c5c3279e578a23e4bf0c44
Heorhii Myronenko <s25954@pjwstk.edu.pl>
2026-03-23 03:23:48 +0100

    Added EquipmentService class

---
+++ apbd-cw2-git-s25954/EquipmentService.cs
@@ -0,0 +1,38 @@
+﻿namespace apbd_cw2_git_s25954;
+
+public class EquipmentService
+{
+    public List<Equipment> Equipment { get; set; }
+
+    public void AddEquipment(Equipment equipment)
+    {
+        Equipment.Add(equipment);
+    }
+
+    public void GetEquipment()
+    {
+        foreach (Equipment equipment in Equipment)
+        {
+            Console.WriteLine(equipment.name + "; " + equipment.description + "; isAvailable: " + equipment.isAvailable);
+        }
+    }
+    
+    public void GetAvailableEquipment()
+    {
+        foreach (Equipment equipment in Equipment)
+        {
+            if (equipment.isAvailable)
+            {
+                Console.WriteLine(equipment.name + "; " + equipment.description + "; isAvailable: " + equipment.isAvailable);
+            }
+        }
+    }
+
+    public void MarkAsUnavailable(Equipment equipment)
+    {
+        equipment.isAvailable = false;
+        Console.WriteLine("Unavailable: " + equipment.name);
+    }
+    
+    
+}
