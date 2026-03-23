d06d00004f5db57c23599a39415b28883b5bac18
Heorhii Myronenko <s25954@pjwstk.edu.pl>
2026-03-23 02:24:55 +0100

    Implemented parent class User and it's child classes Student and Employee

---
+++ apbd-cw2-git-s25954/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace apbd_cw2_git_s25954;
+
+public class Employee : User
+{
+    public override int maxRentals => 5;
+
+    public override string GetUserType()
+    {
+        return "Employee";
+    }
+}
---
+++ apbd-cw2-git-s25954/User.cs
@@ -0,0 +1,21 @@
+﻿namespace apbd_cw2_git_s25954;
+
+public class User
+{
+    private static int IdCounter;
+    
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public virtual int maxRentals { get; set; }
+
+    public User()
+    {
+        this.Id = IdCounter++;
+    }
+
+    public virtual string GetUserType()
+    {
+        return "User";
+    }
+}
