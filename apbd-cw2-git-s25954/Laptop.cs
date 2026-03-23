16df4ccfed0ee65fb5f0c14925974ef1a90efaa8
Heorhii Myronenko <s25954@pjwstk.edu.pl>
2026-03-23 01:37:41 +0100

    Implemented equipment child class Laptop

---
+++ apbd-cw2-git-s25954/Laptop.cs
@@ -0,0 +1,17 @@
+﻿namespace apbd_cw2_git_s25954;
+
+public class Laptop : Equipment
+{
+
+    public string operatingSystem { get; set; }
+    public int ram { get; set; }
+
+
+    public Laptop(string name, string description)
+    {
+        this.name = name;
+        this.isAvailable = true;
+        this.description = description;
+        
+    }
+}
