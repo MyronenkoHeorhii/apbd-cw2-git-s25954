8f74a3e6fe752599c0c5f637b5fae9a008d14ced
Heorhii Myronenko <s25954@pjwstk.edu.pl>
2026-03-23 02:35:15 +0100

    Implemented Rental class

---
+++ apbd-cw2-git-s25954/Rental.cs
@@ -0,0 +1,20 @@
+﻿namespace apbd_cw2_git_s25954;
+
+public class Rental
+{
+    public DateOnly RentalDate { get; set; }
+    public DateOnly DueDate { get; set; }
+    public DateOnly ReturnDate { get; set; }
+    public int Fee{get;set;}
+
+    public User Renter { get; set; }
+    public Equipment RentedEquipment { get; set; }
+
+    public Rental(DateOnly rentalDate, DateOnly dueDate, int fee)
+    {
+        this.RentalDate = rentalDate;
+        this.DueDate = dueDate;
+        this.Fee = fee;
+    }
+    
+}
