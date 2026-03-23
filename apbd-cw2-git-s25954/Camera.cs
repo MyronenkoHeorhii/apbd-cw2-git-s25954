6b3598488b4e5301c463a35334209c50d0efdf30
Heorhii Myronenko <s25954@pjwstk.edu.pl>
2026-03-23 01:52:04 +0100

    Implemented equipment child class Camera

---
+++ apbd-cw2-git-s25954/Camera.cs
@@ -0,0 +1,13 @@
+﻿namespace apbd_cw2_git_s25954;
+
+public class Camera
+{
+    public string brand { get; set; }
+    public int zoom { get; set; }
+
+    public Camera(string brand, int zoom)
+    {
+        this.brand = brand;
+        this.zoom = zoom;
+    }
+}
