accaf1377488188f0313492245c2c84ee0e10f30
Heorhii Myronenko <s25954@pjwstk.edu.pl>
2026-03-23 01:36:56 +0100

    Implemented parent class Equipment

---
+++ apbd-cw2-git-s25954/Equipment.cs
@@ -0,0 +1,16 @@
+﻿namespace apbd_cw2_git_s25954;
+
+public class Equipment
+{
+    private static int idCount;
+    
+    public int id {get;set;}
+    public string name {get;set;}
+    public bool isAvailable {get;set;}
+    public string description {get;set;}
+
+    protected Equipment()
+    {
+        this.id = idCount++;
+    }
+}
